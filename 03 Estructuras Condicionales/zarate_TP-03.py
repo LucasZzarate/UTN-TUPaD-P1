0a682e8631ef6189fe94243c618dbd967004a9cb
Lucas Zarate <zaratelucasmartin1@gmail.com>
2025-04-16 12:41:14 -0300

    Actualizacion del archivo TP3.py

---
+++ 03 Estructuras Condicionales/zarate_TP-03.py
@@ -0,0 +1,12 @@
+edad= int(input("Hola, ingrese su edad."))
+
+if edad > 18 :
+    print("Es mayor de edad.")
+else:
+    pass
+
+nota = float(input("Ingrese su nota, porfavor.")) 
+if nota >= 6.0:
+    print("Aprobado")
+else:
+    print("Desaprobado")
